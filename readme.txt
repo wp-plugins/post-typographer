5fe8990d5935ec5e5359b71fce9b0fb360c6230b
enivid <enivid@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-09-10 17:18:42 +0000

    git-svn-id: https://plugins.svn.wordpress.org/post-typographer/tags/10@287850 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,107 @@
+=== Plugin Name ===
+Tags: typography, formatting, post, posts, plugin
+Requires at least: 2.5
+Tested up to: 3.0.1
+Stable tag: 10
+
+Adds non-breaking spaces, <nobr> tags, common spaces, tags and dashes where needed. Works with English texts only.
+
+== Description ==
+
+When you publish a post or update an already published post, this plugin will
+format the post's content to certain typographic rules:
+
+1. All hyphens between two words (surrounded by spaces) will be transformed to
+m-dashes with the non-breaking spaces ahead of them.
+2. All hyphens between digits (without spaces) will be transformed to n-dashes. 
+But it tries to preserve phone numbers.
+3. All spaces following these words will be replaced with the non-breaking 
+spaces:
+* at
+* or 
+* and
+* the
+* a
+* an
+* in
+* on
+* of
+* for
+* to
+* as
+* i
+* or
+* my
+4. Double and more spaces are replaced with the single ones.
+5. All misplaced spaces near dots, colons, semicolons, exclamation 
+marks and question marks are fixed, where possible.
+6. Composed words with hyphens in them (e.g. "easy-to-use") will be wrapped in
+<nobr></nobr> tags.
+7. <pre></pre> tag and its contents are preserved.
+
+Porblems:
+
+1. The plugin **won't** mess with the HTML tags or other mark-up tags delimited 
+with < and >. It **will** mess with the Javascript code and such constructions 
+as:`
+	[code]
+	bla bla bla
+	[/code]
+`
+Though, since version 6 the mess is minimized for [code] and [video] tags.
+
+2. The n-dash thing will mess up with the phone numbers that have only one 
+hyphen in them. They will be confused with the value ranges.
+
+== Installation ==
+
+1. Upload `typopost.php` to the `/wp-content/plugins/` directory.
+2. Activate the plugin through the 'Plugins' menu in your WordPress Admin section.
+
+== Changelog ==
+
+= 10 =
+* Fixed: Contents of <pre></pre> tag aren't messed up with typographing.
+
+= 9 =
+* Fixed: No longer removes backslashes from posts.
+* Fixed: Tries not to mess with the files' extensions (like .txt).
+
+= 8 =
+* Added: Tries to remove unneded spaces before colons and semicolons without
+messing with the smiles.
+* Added: Word 'by' included to the list of words for `&nbsp;` after them.
+
+= 7 =
+* Fixed: No longer removes spaces before colons and semicolons - not to mess 
+with the smiles.
+
+= 6 =
+* Fixed: Double (and more) warpping with the <nobr> tags.
+* Fixed: <nobr> wrapping and some other formatting in the [code] and [video] 
+embedded text.
+
+= 5 =
+* Added: Wrapping the composed words with hyphens with <nobr> tags.
+
+= 4 =
+* Fixed: Nasty double-space bug on post-updating.
+* Fixed: M-dash placement didn't work in version 3.
+* Fixed: N-dashes are now placed correctly even if the value ranges are near the
+beginning or the end of the text.
+
+= 3 =
+* Added: Replaces misplaced spaces near punctuation marks.
+* Fixed: Phone numbers and dates are now avoided (except single-hyphen phone 
+numbers) when the hyphens in the value ranges are transformed into n-dashes.
+
+= 2 =
+* Added: Replaces 2 or more spaces with a single space in the text.
+* Added: Word 'my' included to the list of words for `&nbsp;` after them.
+
+= 1 =
+* The first working version.
+
+== Thanks to ==
+
+[Typograf by Artemy Lebedev](http://www.artlebedev.ru/tools/typograf/)
---
+++ typopost.php
@@ -0,0 +1,99 @@
+<?php
+/*
+Plugin Name: Post Typographer
+Plugin URI: http://wordpress.org/extend/plugins/post-typographer/
+Description: Formats the text according to typography rules. Works with English texts only.
+Author: Andriy Moraru
+Version: 9
+Author URI: http://www.topforexnews.com
+*/
+
+//Flag to prevent infinite loop when the new post content is saved
+$the_post_is_saving = 0;
+
+function format_typo_post($post_ID)
+{
+	//Loop preventing
+	global $the_post_is_saving;
+	if ($the_post_is_saving) return $post_ID;
+
+	//Don't know if it's required when dealing with publish_post (it is required when dealing with save_post)
+	if (wp_is_post_revision($post_ID) || wp_is_post_autosave($post_ID)) return $post_ID;
+
+	//Prepare parameters to fetch only one post with ID == $post_ID
+	$args = array(
+		'include' => $post_ID,
+		'post_type' => 'any',
+		'post_status' => null
+	); 
+
+	//Fetch post content
+	$posts = get_posts($args);
+	foreach ($posts as $post) $content = $post->post_content;
+
+	//Prevent Wordpress from removing the backslashes
+	$content = str_replace('\\', '\\\\', $content);
+
+	//Get the array of strings made of text without the insides of the <pre> tag
+	$without_pre = preg_split('@<pre[^>]*?>.+</pre>@s', $content);
+    //Get the array of <pre> tags from the text
+	$n_pre = preg_match_all('@<pre[^>]*?>.+</pre>@s', $content, $pre);
+
+	for ($k = 0; $k <= $n_pre; $k++)
+	{
+		//Get the array of strings made of text without HTML tags
+		$without_html = preg_split('@<[\/\!]*?[^<>]*?>@', $without_pre[$k]);
+	    //Get the array of HTML tags from the text
+		$amount = preg_match_all('@<[\/\!]*?[^<>]*?>@', $without_pre[$k], $html);
+
+		//Concatenate the text, making necessary replacements
+		for ($i = 0; $i <= $amount; $i++)
+		{
+		   	//Replace double (and more) spaces with single spaces
+			$without_html[$i] = preg_replace('@ {2,}@', ' ', $without_html[$i]);
+
+			//Add m-dashes with a preceeding non-breaking space, in place of the hyphens, where neeeded
+			$without_html[$i] = str_replace(' - ', '&nbsp;&#8212; ', $without_html[$i]);
+
+			//Get the array of strings made of text without text wrapped with [] tags
+			$without_square = preg_split('@\[(video|code)\].*\[/(video|code)\]@', $without_html[$i]);
+	        //Get the array of [] tags from the text
+			$n_square = preg_match_all('@\[(video|code)\].*\[/(video|code)\]@', $without_html[$i], $square);
+
+			for ($j = 0; $j <= $n_square; $j++)
+			{
+				//Remove space before the punctuation marks that are placed directly after the words
+				$without_square[$j] = preg_replace('@ ((\. )|,|(: )|(; )|!|\?)@', "\$1", $without_square[$j]);
+				//Add space after the punctuation marks where needed
+				$without_square[$j] = preg_replace('@(,|!|\?)([a-z]+)@i', "\$1 \$2", $without_square[$j]);
+				//Add n-dashes in place of hyphens in the numeric ranges, skipping the supposed phone numbers
+				$without_square[$j] = preg_replace('@((^|[^\-^0-9])[0-9]+)-([0-9]+([^0-9^\-]|$))@', "\$1&#8211;\$3", $without_square[$j]);
+				//Wrap composed words with hyphens with <nobr> tag 
+				$without_square[$j] = preg_replace('@(([a-zA-Z]+)(-([a-zA-Z]+))+)@', "<nobr>\$1</nobr>", $without_square[$j]);
+				//Add non-breaking spaces
+				$new_content .= preg_replace("@(?<!')\b(at|or|and|the|a|by|an|in|on|of|for|to|as|i|or|my) @i", "\$1&nbsp;", $without_square[$j]);
+				if ($j < $n_square) $new_content .= $square[0][$j];
+			}
+			if ($i < $amount) $new_content .= $html[0][$i];
+		}
+		if ($k < $n_pre) $new_content .= $pre[0][$k];
+	}
+	//A rather ugly fix for recursive <nobr> wrapping
+	$new_content = preg_replace('@<nobr>(<nobr>)+@', "<nobr>", $new_content);
+	$new_content = preg_replace('@</nobr>(</nobr>)+@', "</nobr>", $new_content);
+
+	//Prepare parameters for updating the post
+	$my_post = array();
+	$my_post['ID'] = $post_ID;
+	$my_post['post_content'] = $new_content;
+ 
+	//Update the post into the database
+	$the_post_is_saving = 1;
+	wp_update_post($my_post);
+	$the_post_is_saving = 0;
+
+	return $post_ID;
+}
+
+add_action('publish_post', 'format_typo_post');
+?>
